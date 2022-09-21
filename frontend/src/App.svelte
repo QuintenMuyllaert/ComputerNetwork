5f28297c91679cab3119bec111c896d158579a43
Quinten Muyllaert <72012305+QuintenMuyllaert@users.noreply.github.com>
2022-09-21 21:55:28 +0200

    📦️ Installed scss

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -25,17 +25,18 @@
 	<p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
 </main>
 
-<style>
+<style lang="scss">
 	.logo {
 		height: 6em;
 		padding: 1.5em;
 		will-change: filter;
-	}
-	.logo:hover {
-		filter: drop-shadow(0 0 2em #646cffaa);
-	}
-	.logo.svelte:hover {
-		filter: drop-shadow(0 0 2em #ff3e00aa);
+		transition: filter 0.2s ease;
+		&:hover {
+			filter: drop-shadow(0 0 2em #646cffaa);
+		}
+		&.svelte:hover {
+			filter: drop-shadow(0 0 2em #ff3e00aa);
+		}
 	}
 	.read-the-docs {
 		color: #888;

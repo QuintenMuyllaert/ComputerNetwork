4bed03ebbb42ccd5aff6ee690a1cacc7075a3b8c
Quinten Muyllaert <72012305+QuintenMuyllaert@users.noreply.github.com>
2022-09-22 13:10:42 +0200

    ⚗️ Test router

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,9 +1,26 @@
 <script lang="ts">
+	import { Router } from "svelte-router-spa";
+
+	import Index from "./pages/index.svelte";
+	import Portfolio from "./pages/portfolio.svelte";
+
+	const routes = [
+		{
+			name: "/",
+			component: Index,
+		},
+		{
+			name: "/portfolio",
+			component: Portfolio,
+		},
+	];
+
 	let text = "Hello World";
 </script>
 
 <main>
 	<h1>{text}!</h1>
+	<Router {routes} />
 </main>
 
 <style lang="scss">

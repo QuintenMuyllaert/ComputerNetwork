d5f96e5662e0cf5617824a9b39f6cb00263654aa
Quinten Muyllaert <quinten.muyllaert@student.howest.be>
2022-09-26 19:31:54 +0200

    💄 Inplement theme picker code

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -3,18 +3,27 @@
 	import routes from "./router";
 
 	import Navbar from "./components/Navbar.svelte";
+
+	//get the preferred theme from the user using the media query
+	const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+	let appTheme = prefersDarkScheme ? "dark" : "light";
+	if (localStorage.getItem("theme")) appTheme = localStorage.getItem("theme");
 </script>
 
-<main>
+<main id="app" class={appTheme}>
 	<Navbar />
 	<Router {routes} />
 </main>
 
 <style lang="scss" scoped>
-	main {
+	main#app {
 		display: grid;
 		grid-template-rows: auto 1fr;
 		width: 100%;
 		height: 100%;
+
+		color: var(--color-text);
+		background-color: var(--color-background);
 	}
 </style>

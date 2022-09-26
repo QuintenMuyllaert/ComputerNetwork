a32713cf9866c24cdbb286d3d19387029423cdee
Quinten Muyllaert <quinten.muyllaert@student.howest.be>
2022-09-26 19:29:15 +0200

    📱 Responsive rain

--- frontend/src/class/Particle.ts
+++ frontend/src/class/Particle.ts
@@ -1,13 +1,12 @@
 export const Particle = class Particle {
-	x: number = 0;
+	x: number = Math.random();
 	y: number = 0;
 	speed: number = 0;
 	alpha: number = 0;
 	size: number = 0;
 	character: string = Math.random() > 0.5 ? "1" : "0";
 
-	constructor(x) {
-		this.x = x;
+	constructor() {
 		this.reset();
 	}
 
@@ -19,6 +18,8 @@ export const Particle = class Particle {
 	}
 
 	draw(ctx) {
+		const width = ctx.canvas.width;
+
 		this.y += this.speed;
 		this.alpha -= 0.01;
 		this.size -= 0.1;
@@ -32,7 +33,9 @@ export const Particle = class Particle {
 		}
 
 		ctx.font = this.size + "px serif";
-		ctx.fillStyle = "rgba(0, 128, 255, " + this.alpha + ")";
-		ctx.fillText(this.character, this.x, this.y);
+		//convert alpha to hex
+		const alphaHex = Math.round(this.alpha * 255).toString(16);
+		ctx.fillStyle = "#3ba7a7" + alphaHex;
+		ctx.fillText(this.character, this.x * width, this.y);
 	}
 };

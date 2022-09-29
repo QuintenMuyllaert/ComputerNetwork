59f7b9c2bd030781aadb4b5246c2288388a03086
Quinten Muyllaert <quinten.muyllaert@student.howest.be>
2022-09-29 14:10:41 +0200

    ⚡️ Use H2 & Spdy

--- proxy/src/index.ts
+++ proxy/src/index.ts
@@ -2,7 +2,8 @@ import dotenv from "dotenv";
 import express, { Request, Response } from "express";
 import mime from "mime-types";
 
-import { createServer } from "https";
+import { createServer } from "spdy";
+
 import { createProxyMiddleware } from "http-proxy-middleware";
 
 dotenv.config();
@@ -65,10 +66,35 @@ app.use(
 
 	await client.close();
 
+	const cips = [
+		"TLS13-AES-256-GCM-SHA384",
+		"TLS13-CHACHA20-POLY1305-SHA256",
+		"TLS_AES_256_GCM_SHA384",
+		"TLS-AES-256-GCM-SHA384",
+		"TLS_CHACHA20_POLY1305_SHA256",
+		"TLS-CHACHA20-POLY1305-SHA256",
+		"ECDHE-ECDSA-AES256-GCM-SHA384",
+		"ECDHE-ECDSA-CHACHA20-POLY1305",
+		"ECDHE-ECDSA-AES256-SHA384",
+		"ECDHE-RSA-AES256-GCM-SHA384",
+		"ECDHE-RSA-CHACHA20-POLY1305",
+		"ECDHE-ECDSA-AES256-SHA",
+		"ECDHE-RSA-AES256-SHA384",
+		"ECDHE-RSA-AES256-SHA",
+	];
+
+	const curves = ["secp521r1", "X448", "secp384r1", "secp256k1"];
+
 	const server = createServer(
 		{
 			key: certificate[0].privateKey,
 			cert: certificate[0].certificate,
+			ciphers: cips.join(":"),
+			ecdhCurve: curves.join(":"),
+			dhparam: certificate[0].dhparam,
+			spdy: {
+				protocols: ["h2", "http/1.1", "http/1.0", "spdy/3.1", "spdy/3", "spdy/2"],
+			},
 		},
 		app,
 	);
@@ -98,6 +124,6 @@ app.use(
 	}, 1000 * 60 * 60 * 24);
 
 	server.listen(443, () => {
-		console.log("Server listening on port 3000");
+		console.log("Server started");
 	});
 })();

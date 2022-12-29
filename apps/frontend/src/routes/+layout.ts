febdfda74b319db1b1753e48d9b2ee964501780b
Quinten Muyllaert <quinten.muyllaert@student.howest.be>
2022-12-29 04:42:19 +0100

    ⚡️ Backend serves Sveltekit build

--- apps/frontend/src/routes/+layout.ts
+++ apps/frontend/src/routes/+layout.ts
@@ -2,3 +2,4 @@ import "../styles/reset.scss";
 import "../styles/app.scss";
 
 export const prerender = true;
+export const trailingSlash = "always";
--- apps/frontend/svelte.config.js
+++ apps/frontend/svelte.config.js
@@ -6,16 +6,12 @@ const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: preprocess(),
-
 	kit: {
 		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
 			pages: "build",
 			assets: "build",
 			precompress: false,
 			strict: false,
-			trailingSlash: "always",
 		}),
 	},
 };
